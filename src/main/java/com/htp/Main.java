93a4aa39a0bc5865caeba5b853e217c2de3bea60
AmintasVikTOR <amintas.vik@gmail.com>
2022-03-28 21:49:56 +0300

    Functional decomposing

--- src/main/java/com/htp/Main.java
+++ src/main/java/com/htp/Main.java
@@ -1,7 +1,24 @@
 package com.htp;
 
+import org.apache.commons.lang3.RandomUtils;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Log's");
+        System.out.println(trueOrFalse());
+        System.out.println(helloWelcome());
+        System.out.println(myRandomDouble());
+
+    }
+
+    public static boolean trueOrFalse() {
+        return RandomUtils.nextBoolean();
+    }
+
+    public static String helloWelcome() {
+        return "returned-helloWelcomeWorld";
+    }
+
+    public static double myRandomDouble() {
+        return RandomUtils.nextDouble();
     }
 }
