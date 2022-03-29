e941d1d2522c4cc2ea2fc9757a81bd12897a398e
AmintasVikTOR <amintas.vik@gmail.com>
2022-03-29 17:13:53 +0300

    SOUT, Master, Clear

--- src/main/java/com/htp/Main.java
+++ src/main/java/com/htp/Main.java
@@ -5,19 +5,8 @@ import org.apache.commons.lang3.RandomUtils;
 public class Main {
     public static void main(String[] args) {
         System.out.println(trueOrFalse());
-        System.out.println(helloWelcome());
-        System.out.println(myRandomDouble());
-        System.out.println(myRandomInteger());
-        System.out.println(myRandomInteger());//chapter2
 
-        System.out.println(myRandomFloat());//chapter3.3
-
-        System.out.println(myRandomFloat());//chapter3.3
-
-        System.out.println(myRandomDouble());//master7
-        System.out.println(myRandomDouble());//master8
-
-        System.out.println(myRandomDouble());//master9
+        // Clear...
 
     }
 
